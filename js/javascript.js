515f31321752c62491af5824df79536ee04a2fda
nasywaaprilia <nasywaaprilia6@gmail.com>
2024-06-26 20:45:30 +0700

    website hypallstore

---
+++ js/javascript.js
@@ -0,0 +1,15 @@
+// toogle class aktif //
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika di klik //
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//klik diluar sidebar utk menghilangkan nav
+
+const menu = document.querySelector("#menu");
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
